e6bc8718811a11e6680c1a3f6ea4d6c6bb35a356
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2023-10-15 22:23:21 +0200

    wall now move based on button

---
+++ Assets/Scripts/WallMover.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class WallMover : MonoBehaviour
+{
+    public GameObject[] walls;
+    public GameObject redButton;
+    public GameObject greenButton;
+    public float moveDistance = 1.0f;
+
+    [SerializeField]
+    private int totalStages = 3;
+    private int currentStage = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == redButton)
+            MoveWallsCloser();
+
+        else if (collision.gameObject == greenButton)
+            MoveWallsAway();
+    }
+
+    public void MoveWallsCloser()
+    {
+        if (currentStage < totalStages)
+        {
+            foreach (GameObject wall in walls)
+            {
+                Vector3 directionToOrigin = (Vector3.zero - wall.transform.position).normalized;
+                wall.transform.position += directionToOrigin * moveDistance;
+            }
+            currentStage++;
+        }
+    }
+
+    public void MoveWallsAway()
+    {
+        if (currentStage > 0)
+        {
+            foreach (GameObject wall in walls)
+            {
+                Vector3 directionToOrigin = (Vector3.zero - wall.transform.position).normalized;
+                wall.transform.position -= directionToOrigin * moveDistance;
+            }
+            currentStage--;
+        }
+    }
+}
